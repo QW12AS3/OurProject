9a6abf460960d9e5ae06a6cb7fa95547a2ff248b
Omar Zaghlouleh <zaomar56@gmail.com>
2022-06-03 18:06:30 +0300

    Home Page

---
+++ lib/view_models/mobile_home_view_model.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/cupertino.dart';
+import 'package:home_workout_app/models/workout_model.dart';
+
+class MobileHomeViewModel with ChangeNotifier {
+  //temporary
+  Map<String, bool> categories = {
+    'Recommended': true,
+    'All': false,
+    'Arm': false,
+    'Chest': false,
+    'Back': false,
+    'Leg': false,
+  };
+
+  //temporary
+  List<Map<dynamic, dynamic>> workouts = [
+    {
+      'id': 1,
+      'data': {
+        'name': 'Arm',
+        'categorie': 'Arm',
+        'excersises': 7,
+        'publisher': 'Ahmad',
+        'excpectedTime': 50,
+        'imageUrl':
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/full-body-workout-1563458040.jpg'
+      },
+    },
+    {
+      'id': 5,
+      'data': {
+        'name': 'Arm',
+        'categorie': 'Arm',
+        'excersises': 7,
+        'publisher': 'Ahmad',
+        'excpectedTime': 50,
+        'imageUrl':
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/full-body-workout-1563458040.jpg'
+      },
+    },
+    {
+      'id': 2,
+      'data': {
+        'name': 'Arm',
+        'categorie': 'Leg',
+        'excersises': 7,
+        'excpectedTime': 50,
+        'publisher': 'Ahmad',
+        'imageUrl':
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/full-body-workout-1563458040.jpg'
+      }
+    },
+    {
+      'id': 3,
+      'data': {
+        'name': 'Arm',
+        'categorie': 'All',
+        'excersises': 7,
+        'excpectedTime': 50,
+        'publisher': 'Ahmad',
+        'imageUrl':
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/full-body-workout-1563458040.jpg'
+      }
+    },
+    {
+      'id': 4,
+      'data': {
+        'name': 'Arm',
+        'categorie': 'Recommended',
+        'excersises': 7,
+        'excpectedTime': 50,
+        'publisher': 'Ahmad',
+        'imageUrl':
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/full-body-workout-1563458040.jpg'
+      }
+    },
+    {
+      'id': 4,
+      'data': {
+        'name': 'Arm',
+        'categorie': 'Recommended',
+        'excersises': 7,
+        'excpectedTime': 50,
+        'publisher': 'Ahmad',
+        'imageUrl':
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/full-body-workout-1563458040.jpg'
+      }
+    },
+    {
+      'id': 4,
+      'data': {
+        'name': 'Arm',
+        'categorie': 'Recommended',
+        'excersises': 7,
+        'excpectedTime': 50,
+        'publisher': 'Ahmad',
+        'imageUrl':
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/full-body-workout-1563458040.jpg'
+      }
+    }
+  ];
+
+  List<WorkoutModel> getWorkouts() {
+    List<WorkoutModel> workoutsList = [];
+    for (int i = 0; i < workouts.length; i++) {
+      workoutsList.add(WorkoutModel.fromJson(workouts[i]));
+    }
+    return workoutsList;
+  }
+
+  changeSelectedCategorie(key, selectedValue) {
+    categories.updateAll((key, value) => false);
+    categories.update(key, (value) => selectedValue);
+    notifyListeners();
+  }
+}
--- lib/views/Home View/Mobile/mobile_home_view_widgets.dart
+++ lib/views/Home View/Mobile/mobile_home_view_widgets.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:home_workout_app/constants.dart';
+import 'package:shimmer/shimmer.dart';
 
 class buildSummaryRow extends StatelessWidget {
   buildSummaryRow({required this.title1, required this.title2, Key? key})
@@ -18,6 +20,26 @@ class buildSummaryRow extends StatelessWidget {
         ),
         Text(title2, style: theme.textTheme.bodySmall)
       ],
+    );
+  }
+}
+
+class LoadingContainer extends StatelessWidget {
+  const LoadingContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context).size;
+
+    return Shimmer.fromColors(
+      baseColor: Colors.grey.shade400,
+      highlightColor: Colors.grey.shade200,
+      child: Container(
+        width: mq.width * 0.95,
+        height: 250,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15), color: Colors.white),
+      ),
     );
   }
 }
