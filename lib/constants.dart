fd407207771cd6fa69d13122004b4f17a18bd1aa
Omar Zaghlouleh <zaomar56@gmail.com>
2022-07-02 00:38:19 +0300

    choose image dialog & translate edit view

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,6 +6,10 @@ Color greyColor = Colors.grey;
 
 const String base_URL = 'https://reqres.in/api';
 
+const String apiKey =
+    'THSzx8cmJny4DFmjvjX2calOKSduaJxb3YKC9sCuoCdEiF4J9w6qul5kRFwt1mUR';
+const String url = 'http://192.168.0.1:8000/api';
+
 enum Gender { male, female }
 
 enum Units { kg, lb, cm, ft }
